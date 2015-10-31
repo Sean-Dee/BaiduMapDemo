dcb62b18d4fd5c05ac677315e3c362f6ab0e3da2
sean.dee <sean.dee.it@gmail.com>
2015-10-31 14:06:13 +0800

    init

---
+++ app/src/main/java/com/dee/sean/baidumapdemo/MainActivity.java
@@ -0,0 +1,203 @@
+package com.dee.sean.baidumapdemo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class
+        MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+
+    private Context context;
+
+    //定位相关
+    private LocationClient mLocationClient;
+    private MyLocationListener mLocationListener;
+    private boolean isFirstIn = true;
+
+    private double mLatitude;
+    private double mLongtitude;
+
+    //自定义定位图标
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        this.context = this;
+
+        initView();
+
+        initLocation();
+    }
+
+    private void initLocation() {
+        mLocationClient = new LocationClient(this);
+        mLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(mLocationListener);
+
+        LocationClientOption option = new LocationClientOption();
+        option.setCoorType("bd09ll");
+        option.setIsNeedAddress(true);
+        option.setOpenGps(true);
+        option.setScanSpan(3000);
+
+        mLocationClient.setLocOption(option);
+
+    }
+
+    private void initView() {
+        mMapView = (MapView) findViewById(R.id.id_bmapView);
+        mBaiduMap = mMapView.getMap();
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+        mBaiduMap.setMapStatus(msu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.id_map_common:
+                mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.id_map_site:
+                mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.id_map_traffic:
+                if (mBaiduMap.isTrafficEnabled()) {
+                    mBaiduMap.setTrafficEnabled(false);
+                    item.setTitle("实时交通(On)");
+                } else {
+                    mBaiduMap.setTrafficEnabled(true);
+                    item.setTitle("实时交通(Off)");
+                }
+                break;
+            case R.id.id_map_location:
+                centerToMyLocation();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 定位到我的位置
+     */
+    private void centerToMyLocation() {
+        LatLng latLng = new LatLng(mLatitude, mLongtitude);
+        MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+        mBaiduMap.animateMapStatus(msu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Start location
+        mBaiduMap.setMyLocationEnabled(true);
+        if (!mLocationClient.isStarted()) {
+            mLocationClient.start();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        //Stop location
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            MyLocationData data = new MyLocationData.Builder()
+                    .accuracy(bdLocation.getRadius())
+                    .latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLongitude())
+                    .build();
+
+            mBaiduMap.setMyLocationData(data);
+
+//            MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL,);
+
+            mLatitude = bdLocation.getLatitude();
+            mLongtitude = bdLocation.getLongitude();
+
+            if (isFirstIn) {
+
+                LatLng latLng = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+                MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+                mBaiduMap.animateMapStatus(msu);
+
+                isFirstIn = false;
+            }
+
+            Toast.makeText(context, bdLocation.getAddrStr(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
